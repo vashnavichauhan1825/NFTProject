52b3e498ca3d126b3907c9c075480930546ea424
vashnavi18 <vashnavichauhan1@gmail.com>
2024-08-11 00:40:04 +0530

    optimize: api's

--- src/services/service.js
+++ src/services/service.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
-
-const API_TOKEN = 'cqt_rQ4rjPvbKdDRJCTVHyDWxdhH4hPp';
-const CONTRACT_ADDRESS = '0x8821bee2ba0df28761afff119d66390d594cd280';
+import {API_TOKEN, CONTRACT_ADDRESS, BASE_URL} from '@env';
 
 export const fetchNFTs = async (page = 0, pageSize = 10) => {
-  const url = `https://api.covalenthq.com/v1/eth-mainnet/nft/${CONTRACT_ADDRESS}/metadata/?page-size=${pageSize}&page-number=${page}`;
+  const url = `${BASE_URL}/v1/eth-mainnet/nft/${CONTRACT_ADDRESS}/metadata/?page-size=${pageSize}&page-number=${page}`;
   const response = await axios.get(url, {
     headers: {
       'Content-Type': 'application/json',
     },
     auth: {
       username: API_TOKEN,
-      password: '',
     },
   });
 
